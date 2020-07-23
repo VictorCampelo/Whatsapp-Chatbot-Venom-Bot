f9d5db50f83e84761da932cb7bd484f610231852
Victor Campelo <victor_campelo@outlook.com>
2020-07-23 20:27:07 -0300

    fix: updated value global var to each solicitation.

--- src/stages/4.js
+++ src/stages/4.js
@@ -1,6 +1,6 @@
 const banco = require("../banco");
-let estagioInterno = 0;
 
+let estagioInterno = 0;
 function execute(user, msg) {
   //banco.db[user].stage = 0;
 
@@ -10,14 +10,16 @@ function execute(user, msg) {
     banco.db[user].valor_troco += parseFloat(msg) - banco.db[user].preco
 
     //ERRORS VERIFICATIONS
-
+    estagioInterno = 0
     return stages.step[5].obj.execute(user, "");
   }
+
   if (msg === "1") {
     estagioInterno++;
 
     return ["Digite o valor em dinheiro para levarmos o troco: "];
   }
+  
   return ["Escolha a forma de pagamento:\n1️⃣-Dinheiro\n2️⃣-Cartão"];
 }
 
