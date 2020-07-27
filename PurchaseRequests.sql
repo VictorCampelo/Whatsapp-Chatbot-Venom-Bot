e476d4cd9d1d6ca72e1c29d3a65a9cfd8f51f876
Victor Campelo <victor_campelo@outlook.com>
2020-07-27 19:45:22 -0300

    remove unnecessary files

--- PurchaseRequests.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `teste`.`pedidos` (
-  `id` INT NOT NULL,
-  `numero` INT NULL,
-  `nome` VARCHAR(200) NOT NULL,
-  `endereco` VARCHAR(200) NULL,
-  `preco` FLOAT NOT NULL,
-  `valor_troco` FLOAT NULL,
-  PRIMARY KEY (`id`));
