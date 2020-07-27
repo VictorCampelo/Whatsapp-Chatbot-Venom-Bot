31a02b216d3d393e0c4b2ce1bedb4c780c048e20
Victor Campelo <victor_campelo@outlook.com>
2020-07-27 19:35:28 -0300

    client model

---
+++ src/database/migrations/20200727160923-create-client.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, DataTypes) => {
+    return queryInterface.createTable('Client', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      numero: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        unique: true,
+      },
+      endereco: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      preco: {
+        allowNull: false,
+        type: DataTypes.FLOAT,
+      },
+      valor_troco: {
+        allowNull: false,
+        type: DataTypes.FLOAT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('Client');
+  }
+};
