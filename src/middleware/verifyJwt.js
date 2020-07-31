d8600a5f1023e3ef0b04920142f6b00d1d91576a
Victor Campelo <victor_campelo@outlook.com>
2020-07-30 23:40:05 -0300

    feat: create jwt middle-ware to sings accesses and private routes request

--- src/middleware/verifyJwt.js
+++ src/middleware/verifyJwt.js
@@ -1,18 +1,105 @@
-const jwt = require('jsonwebtoken');
+const JWT = require('jsonwebtoken')
+const createError = require('http-errors')
+const client = require('../helpers/init_redis')
 
-module.exports = (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    const userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
-      throw 'Invalid user ID';
-    } else {
-      next();
+module.exports = {
+  verifyAccessToken: (req, res, next) => {
+    const token = req.headers.authorization;
+    const secret = process.env.ACCESS_TOKEN_SECRET
+    
+    if(!token) res.status(401).json("Access Danied")
+    try {
+      JWT.verify(
+        token,
+        process.env.REFRESH_TOKEN_SECRET,
+        (err, user) => {
+          if (err) {
+            return res.status(401).json(err)
+          }
+          const userId = user.id
+          client.GET(userId, (err, result) => {
+            if (err) {
+              console.log(err.message)
+              return res.status(401).json(err)
+            }
+            if (token === result) { 
+              next();
+            }else res.status(401).json(createError.Unauthorized())
+          })
+        }
+      )
+    } catch (error) {
+      res.status(400).json("Invalid Token")
     }
-  } catch {
-    res.status(401).json({
-      error: new Error('Invalid request!')
+  },
+  
+  signAccessToken: (params) => {
+    return new Promise((resolve, reject) => {
+      JWT.sign(
+        params, 
+        process.env.ACCESS_TOKEN_SECRET, 
+        {expiresIn: '1d'},
+        (err, token) => {
+          if (err) {
+            console.log(err.message)
+            reject(createError.InternalServerError())
+            return
+          }
+          resolve(token);
+        });
     });
-  }
-};
+  },
+
+  signRefreshToken: (params) => {
+    return new Promise((resolve, reject) => {
+      const secret = process.env.REFRESH_TOKEN_SECRET
+      const options = {
+        expiresIn: '1y',
+        issuer: 'pickurpage.com',
+      }
+      JWT.sign(params, secret, options, (err, token) => {
+        if (err) {
+          console.log(err.message)
+          // reject(err)
+          reject(createError.InternalServerError())
+          return 
+        }
+        // resolve(token)
+        const userId = params.id
+        console.log(token)
+        client.SET(userId, token, 'EX', 365 * 24 * 60 * 60, (err, reply) => {
+          if (err) {
+            console.log(err.message)
+            reject(createError.InternalServerError())
+            return
+          }
+          resolve(token)
+        })
+      })
+    })
+  },
+
+  verifyRefreshToken: (refreshToken) => {
+    return new Promise((resolve, reject) => {
+      JWT.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        (err, user) => {
+          if (err) {
+            return reject(createError.Unauthorized())
+          }
+          const userId = user.id
+          client.GET(userId, (err, result) => {
+            if (err) {
+              console.log(err.message)
+              reject(createError.InternalServerError())
+              return
+            }
+            if (refreshToken === result) return resolve(userId)
+            reject(createError.Unauthorized())
+          })
+        }
+      )
+    })
+  },
+}
