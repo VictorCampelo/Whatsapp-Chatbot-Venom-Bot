091bb2552998bb227d9dd9e0ca7d0789fc2b0b65
Victor Campelo <victor_campelo@outlook.com>
2020-07-30 23:33:30 -0300

    feat: add logout and refresh token routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,13 +2,49 @@ var express = require('express');
 const fs = require("fs")
 const routesAuth = express.Router();
 const userController = require('../controllers/userController');
+const { nextTick } = require('process');
+const { create } = require('../models/User');
+const createHttpError = require('http-errors');
+const { verifyRefreshToken, signAccessToken, signRefreshToken } = require('../middleware/verifyJwt');
+const client = require('../helpers/init_redis')
 
 routesAuth.post('/login',  userController.login);
 
-routesAuth.post('/logout', function(req, res) {
-    res.json({ auth: false, token: null });
+routesAuth.post('/register', userController.create);
+
+routesAuth.delete('/logout', async function(req, res, next) {
+    try {
+        const refreshToken = req.body
+        if (!refreshToken) throw createHttpError.BadRequest()
+        const userId = await verifyRefreshToken(refreshToken.refresh_token)
+        client.DEL(userId, (err, val) => {
+          if (err) {
+            console.log(err.message)
+            throw createError.InternalServerError()
+          }
+          console.log(val)
+          res.sendStatus(204)
+        })
+      } catch (error) {
+        next(error)
+      }
 })
 
-routesAuth.post('/register', userController.create);
+routesAuth.post('/refresh-token', async function(req, res, next) {
+    try {
+        const refresh = req.body
+        if (!refresh) {
+            throw createHttpError.BadRequest()
+        }
+        const userId = await verifyRefreshToken(refresh.refresh_token)
+
+        const accessToken = await signAccessToken({id: userId})
+        const refreshToken = await signRefreshToken({id: userId})
+
+        res.status(200).json({"accessToken": accessToken, "refreshToken": refreshToken})
+    } catch (error) {
+        next(error)
+    }
+})
 
 module.exports = routesAuth;
